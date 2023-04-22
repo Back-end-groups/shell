30b1e5ec0e240494d2de57328c3c6c75668acfbc
huzixin <huzixin@qq.com>
2023-04-22 20:27:44 +0800

    七牛云图片

---
+++ src/main/java/com/improve/shell/controller/ToolController.java
@@ -0,0 +1,43 @@
+package com.shuai.controller;
+
+
+import com.improve.shell.util.QiniuUtils;
+import com.improve.shell.util.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+/**
+ * @Author: fengxin
+ * @CreateTime: 2022-11-29  19:49
+ * @Description: 工具控制器：用于文件上传、短信验证、邮箱发送...等
+ */
+@Slf4j
+@Transactional
+@RestController
+@RequestMapping("/tool")
+public class ToolController {
+
+    @Autowired
+    private QiniuUtils qiniuUtils;
+
+    @PostMapping("/upload")
+    public Result upload(@RequestBody MultipartFile file) throws IOException {
+        log.info("传过来的file==>{}",file);
+        //根据文件输入流原文件名获取url
+        String url = qiniuUtils.upload(file.getInputStream(), file.getOriginalFilename());
+        return Result.success("文件上传成功",url);
+    }
+
+    @DeleteMapping("/delete")
+    public Result delete(@RequestBody MultipartFile file) throws IOException{
+        log.info("传过来的file==>{}",file);
+        //根据原文件名称删除七牛云的文件
+        qiniuUtils.delete(file.getOriginalFilename());
+        return Result.success("删除成功");
+    }
+}
