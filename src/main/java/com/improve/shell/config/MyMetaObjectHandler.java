a969dc3ced36c68b730bcd9097c667e3359806ad
huzixin <1835759151@qq.com>
2023-04-24 15:31:15 +0800

    为表的共同属性赋值

---
+++ src/main/java/com/improve/shell/config/MyMetaObjectHandler.java
@@ -0,0 +1,32 @@
+package com.improve.shell.config;
+
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * 自动填充字段，将表的共同属性共同赋值
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        metaObject.setValue("listing", LocalDateTime.now());
+
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+//        long id = Thread.currentThread().getId();
+//        log.info("线程id: {}",id);
+//         metaObject.setValue("updateTime",LocalDateTime.now());
+
+
+    }
+}
