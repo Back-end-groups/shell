0550881694aa4ecabd383e3000dd72fe666bfdc2
huzixin <1835759151@qq.com>
2023-04-25 15:48:55 +0800

    房屋拓展类

---
+++ src/main/java/com/improve/shell/pojo/vo/HouseVO.java
@@ -0,0 +1,14 @@
+package com.improve.shell.pojo.vo;
+
+import com.improve.shell.pojo.po.House;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class HouseVO extends House {
+    List<String> images = new ArrayList<>(); //图片集
+    private String username; //用户名
+
+}
