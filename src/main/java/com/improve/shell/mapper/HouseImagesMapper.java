46237637a057363468d196a525b692876704b412
huzixin <1835759151@qq.com>
2023-04-25 15:45:11 +0800

    mapper接口

---
+++ src/main/java/com/improve/shell/mapper/HouseImagesMapper.java
@@ -0,0 +1,10 @@
+package com.improve.shell.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.improve.shell.pojo.po.HouseImages;
+import com.improve.shell.pojo.vo.HouseVO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface HouseImagesMapper extends BaseMapper<HouseImages> {
+}
