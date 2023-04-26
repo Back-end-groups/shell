2b7f487d81cab7cb9d06e83bc8d51bf91172682f
huzixin <1835759151@qq.com>
2023-04-26 23:09:40 +0800

    用户和房屋关系的Mapper类

---
+++ src/main/java/com/improve/shell/mapper/HouseWithUserMapper.java
@@ -0,0 +1,9 @@
+package com.improve.shell.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.improve.shell.pojo.po.HouseWithUser;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface HouseWithUserMapper extends BaseMapper<HouseWithUser> {
+}
