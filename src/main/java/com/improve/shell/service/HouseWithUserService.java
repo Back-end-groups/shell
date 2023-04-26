ff2614b4425c15dd8463953b85bd79c645ef676b
huzixin <1835759151@qq.com>
2023-04-26 23:10:01 +0800

    用户和房屋关系的service类

---
+++ src/main/java/com/improve/shell/service/HouseWithUserService.java
@@ -0,0 +1,7 @@
+package com.improve.shell.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.improve.shell.pojo.po.HouseWithUser;
+
+public interface HouseWithUserService extends IService<HouseWithUser> {
+}
