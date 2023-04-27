6d6c0ae1c32edb7cef6cf9f5426fbef0adb9fa13
huzixin <1835759151@qq.com>
2023-04-27 18:39:34 +0800

    添加了属性

--- src/main/java/com/improve/shell/pojo/po/HouseWithUser.java
+++ src/main/java/com/improve/shell/pojo/po/HouseWithUser.java
@@ -10,4 +10,10 @@ public class HouseWithUser {
     private String houseId;
     //用户id
     private Long uid;
+
+    //判断是否是用户自己发表的
+    private Boolean isOwn;
+
+    //判断用户是否收藏这个房屋信息
+    private Boolean isCollect;
 }
